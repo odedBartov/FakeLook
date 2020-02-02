fadf839ba8da50401053957a0d25ce7c8f4049af
odedbartov <odedoded777@gmail.com>
2020-02-02 18:05:15 +0200

    Update DBService.js

--- fakeLook-service/friends/DBService.js
+++ fakeLook-service/friends/DBService.js
@@ -1,19 +1,15 @@
 const sql = require('mssql')
 
-const services = {  }
-module.exports = services
+const config = require('../credentials')
 
-const config = {
-    server: 'localhost',
-    database: 'UsersDB',
-    user: 'oded',
-    password: '1234'
+const dbPool = new sql.ConnectionPool(config, err => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('connected to DB!')
+  }
+})
+
+module.exports = {
+    
 }
-
-// const dbPool = new sql.ConnectionPool(config, err => {
-//   if (err) {
-//     console.log(err)
-//   } else {
-//     console.log('connected to DB!')
-//   }
-// })
