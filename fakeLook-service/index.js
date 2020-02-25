84bfbd2e58ecfc0b2eabee177f88b1af61873936
mushky <mushmush722@gmail.com>
2020-02-25 14:20:11 +0200

    change

--- fakeLook-service/index.js
+++ fakeLook-service/index.js
@@ -6,7 +6,6 @@ const fs = require('fs')
 const cors = require('cors')
 
 const authController = require('./authentication/authController')
-<<<<<<< HEAD
 const postsController = require('./social/posts/postsController')
 const friendsController = require('./social/friends/friendsController')
 const errorHandler = require('./common/errorHandler')
@@ -16,23 +15,20 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Expose-Headers', '*')
     next()
 })
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//     )
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Expose-Headers', '*')
-//     next()
-// })
 
 app.use(bp.json())
 
 app.use((req, res, next) => {
     errorHandler.setNext(next)
 })
+/* const socialController = require('./social/socialController') */
+const bodyParser = require('body-parser');
+
+/* const FRIENDaPI = require('./friends/DBService');
+ FRIENDaPI.InsertFreind(2,3).then(res=>console.log(res)) */
+//convertion of body to json object
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
@@ -42,45 +38,12 @@ app.use('/posts', postsController)
 
 app.use('/friends', friendsController)
 
+/* app.use('/social', socialController) */
 
 app.use((err, req, res, next) => {
     fs.appendFile('Log.txt', `Error thrown at ${new Date()}${err}` + "\n\n\n", () => { })
-=======
-const postsController = require('./posts/postsController')
-const friendsController = require('./friends/friendsController')
-const socialController = require('./social/socialController')
-
-const bodyParser = require('body-parser');
-
-/* const FRIENDaPI = require('./friends/DBService');
- FRIENDaPI.InsertFreind(2,3).then(res=>console.log(res)) */
- 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-    )
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    next()
-})
-//convertion of body to json object
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-app.use('/authentication', authController)
-
-app.use('/posts', postsController)
-
-app.use('/friends', friendsController)
-
-app.use('/social', socialController)
-
 
 app.use((err, req, res, next) => {
->>>>>>> freinds
     res.status(err.status ? err.status : 500).send(err.message)
 })
 
