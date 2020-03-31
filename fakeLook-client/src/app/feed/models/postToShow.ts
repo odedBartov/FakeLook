8bb05c769a98915eab6633331004fde86aa320a8
mushky <mushmush722@gmail.com>
2020-03-31 19:52:15 +0300

    commit

--- fakeLook-client/src/app/feed/models/postToShow.ts
+++ fakeLook-client/src/app/feed/models/postToShow.ts
@@ -1,11 +1,5 @@
 export class postToShow{
     post_id: string;
     image_url: string;
-<<<<<<< HEAD
     location: {lon: number, lat: number}
-=======
-    latitude: number;
-    longitude: number;
-    text:string;
->>>>>>> change
 }
