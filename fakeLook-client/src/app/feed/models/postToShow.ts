1bd9721e02bee48a909aba679735bd011e328524
odedBartov <37040795+odedBartov@users.noreply.github.com>
2020-03-31 20:04:15 +0300

    text to post to show model

--- fakeLook-client/src/app/feed/models/postToShow.ts
+++ fakeLook-client/src/app/feed/models/postToShow.ts
@@ -1,11 +1,6 @@
 export class postToShow{
     post_id: string;
     image_url: string;
-<<<<<<< HEAD
-    location: {lon: number, lat: number}
-=======
-    latitude: number;
-    longitude: number;
-    text:string;
->>>>>>> change
+    location: {lon: number, lat: number};
+    text: string
 }
