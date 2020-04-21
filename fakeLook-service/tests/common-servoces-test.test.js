a8b794fadea01401a746f75162b29a1cd1977522
odedBartov <37040795+odedBartov@users.noreply.github.com>
2020-04-21 14:21:28 +0300

    restore framework

---
+++ fakeLook-service/tests/common-servoces-test.test.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert
+const container = require('./containerConfigForTests')
+const jwtService = container.get('JWTservice')
+
+describe("the tests for common services", () => {
+    it("test the jwt service", () => {        
+        const id = "some_id"
+        const token = jwtService.createToken(id)
+        jwtService.validateToken(token, (err, data) => {
+            if (err) {
+                assert.Throw(err)
+            }
+            else{
+                assert.equal(data.id, id)
+            }
+        })
+    })
+})
---
+++ fakeLook-service/tests/testDAOs/authentication.js
@@ -0,0 +1,59 @@
+const sql = require('mssql')
+
+class authenticationDAO {
+    elasticClient
+    UUID
+    dbpool
+    constructor(authenticationConfig, idCreator) {
+        this.UUID = idCreator
+        this.dbPool = new sql.ConnectionPool(authenticationConfig, err => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('connected to DB from Authentication!')
+            }
+        })
+    }
+
+    CheckIfUserExist (userName, callback) {
+        // const dbreq = this.dbPool.request()
+        // dbreq.input('UserName', sql.NVarChar(50), userName)
+        // dbreq.execute('SP_CheckUserName', (err, data) => {
+        //     if (err) {
+        //         callback(err, undefined)
+        //     } else {
+        //         callback(undefined, data.recordset[0])
+        //     }
+        // })
+    }
+
+
+    InsertUser (user, callback) {
+        // const dbreq = this.dbPool.request()
+        // dbreq.input('ID', sql.NVarChar(60), user.ID)
+        // dbreq.input('UserName', sql.NVarChar(20), user.userName)
+        // dbreq.input('Password', sql.NVarChar(60), user.password)
+        // dbreq.input('email', sql.NVarChar(30), user.email)
+        // dbreq.execute('SP_InsertUser', (err, data) => {
+        //     if (err) {
+        //         callback(err, undefined)
+        //     } else {
+        //         callback(undefined, data.recordset[0])
+        //     }
+        // })
+    }
+
+    GetPassword (userName, callback) {
+        // const dbreq = this.dbPool.request()
+        // dbreq.input('UserName', sql.NVarChar(15), userName)
+        // dbreq.execute('SP_GetPassword', (err, data) => {
+        //     if (err) {
+        //         callback(err, undefined)
+        //     } else {
+        //         callback(undefined, data.recordset[0]? data.recordset[0] : {Password: ''})
+        //     }
+        // })
+    }
+}
+
+module.exports = authenticationDAO 
